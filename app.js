51036dc707387fb344ebdfcc95f07bb28010ec41
Joyster Rodrigues <77756392+joysterr@users.noreply.github.com>
2024-03-29 23:52:47 +0000

    add login and logout functionality
    added login and logout routes
    updated index route and index ejs view page to accomodate login feature
    added localstrategy based authentication using passportjs and express session
    added necessary middleware in app.js to enable login and logout

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require('express')
 const session = require('express-session')
 const logger = require('morgan')
+const bcrypt = require('bcryptjs')
 const passport = require('passport')
 const LocalStrategy = require('passport-local')
-const bcrypt = require('bcryptjs')
 const mongoose = require('mongoose')
 
 const indexRouter = require('./routes/index')
 const signupRouter = require('./routes/signup')
+const loginRouter = require('./routes/login')
+const logoutRouter = require('./routes/logout')
+
+const User = require('./models/User')
 
 const app = express()
 const port = 3000
@@ -27,9 +31,43 @@ app.use(express.urlencoded({ extended: false }))
 app.use(logger('dev'))
 app.use(express.static('public'))
 
+// auth middleware using passport
+passport.use(
+    new LocalStrategy(async (username, password, done) => {
+        try {
+            const user = await User.findOne({ username: username })
+            if (!user) {
+                return done(null, false, { message: 'Incorrect username' })
+            }
+            const match = await bcrypt.compare(password, user.password)
+            if (!match) {
+                return done(null, false, { message: 'Incorrect password' })
+            }
+            return done(null, user)
+        } catch (err) {
+            return done(err)
+        }
+    })
+)
+
+passport.serializeUser((user, done) => {
+    done(null, user.id)
+})
+
+passport.deserializeUser(async (id, done) => {
+    try {
+        const user = await User.findById(id)
+        done(null, user)
+    } catch (err) {
+        done(err)
+    }
+})
+
 // routes
 app.use('/', indexRouter)
 app.use('/signup', signupRouter)
+app.use('/login', loginRouter)
+app.use('/logout', logoutRouter)
 
 // start app
 app.listen(port, () => console.log(`dev: server running on localhost:${port}`))
