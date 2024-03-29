f4436e727ee809a1ac52ae643805739cd17539ab
Joyster Rodrigues <77756392+joysterr@users.noreply.github.com>
2024-03-29 23:13:31 +0000

    add basic css styling
    updated index and signup page
    added simple css style sheet

--- routes/signup.js
+++ routes/signup.js
@@ -1,12 +1,37 @@
 const express = require('express')
+const bcrypt = require('bcryptjs')
 const router = express.Router()
+
+const User = require('../models/User')
 
 router.get('/', (req, res) => {
     res.render('signup')
 })
 
-router.post('/', (req, res) => {
-    res.send('user signup post req')
+router.post('/', async (req, res) => {
+    try {
+        const user = User.findOne({ username: req.body.username }).exec()
+
+        if (user) {
+            res.render('signup', { error: 'User exists. Try Loggin in' })
+        }
+
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+
+        const newUser = new User({
+            username: req.body.username,
+            password: hashedPassword
+        })
+
+        const result = await newUser.save()
+
+        if (result) {
+            res.redirect('/')
+        }
+    } catch (error) {
+        console.error(error)
+    }
+
 })
 
 module.exports = router
