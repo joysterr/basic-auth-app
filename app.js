3d0c96cf6579e0269af40606cd8ea8f5478c69f6
Joyster Rodrigues <77756392+joysterr@users.noreply.github.com>
2024-03-29 19:32:08 +0000

    add basic router and index views page
    imported required middleware
    created routes folder containing index route
    created index ejs page displaying basic barebones index page

--- app.js
+++ app.js
@@ -1,7 +1,33 @@
 const express = require('express')
+const session = require('express-session')
+const logger = require('morgan')
+const passport = require('passport')
+const LocalStrategy = require('passport-local')
+const bcrypt = require('bcryptjs')
+const mongoose = require('mongoose')
+
+const indexRouter = require('./routes/index')
+
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World'))
+//db setup
+mongoose.connect(process.env.DB_URI)
+    .then(console.log('dev: db connected'))
+    .catch(err => console.error(err))
 
+// view engine
+app.set('view engine', 'ejs')
+
+// middleware
+app.use(session({ secret: "cats", resave: false, saveUninitialized: true }))
+app.use(passport.session())
+app.use(express.urlencoded({ extended: false }))
+app.use(logger('dev'))
+app.use(express.static('public'))
+
+// routes
+app.get('/', indexRouter)
+
+// start app
 app.listen(port, () => console.log(`dev: server running on localhost:${port}`))
